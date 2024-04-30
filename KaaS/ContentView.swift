0201d7b6e5eff96b4965ea2a3f0cdf13bd542a98
DevCache1 <martynasbaranskas@gmail.com>
2024-04-30 11:47:13 +0300

    Final Commit
    - Simple API request to fetch a quote on button press + on app launch. Fetches famous Kanye quotes from https://api.kanye.rest

--- KaaS/ContentView.swift
+++ KaaS/ContentView.swift
@@ -8,15 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var quote: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("Kanye")
+            
+            Text("KaaS")
+                .font(.title)
+                .bold()
+            Text("Kanye as a Service")
+            
+            
+            Spacer()
+            
+            Text("\"\(quote)\"")
+                .padding()
+                .italic()
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            
+            Button("Generate Quote", action: {fetchQuote()})
+                .padding()
+                .background(.green)
+                .foregroundColor(.white)
+                .cornerRadius(20)
+
+        }
+        .onAppear {
+            fetchQuote()
         }
         .padding()
     }
+    
+    func fetchQuote() {
+        guard let url = URL(string: "https://api.kanye.rest") else {
+            print("Invalid URL")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                print("Error: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            
+            do {
+                let quoteResponse = try JSONDecoder().decode(QuoteResponse.self, from: data)
+                DispatchQueue.main.async {
+                    self.quote = quoteResponse.quote
+                }
+            } catch {
+                print("Error decoding JSON: \(error)")
+            }
+        }.resume()
+    }
+}
+
+struct QuoteResponse: Codable {
+    let quote: String
 }
 
 #Preview {
