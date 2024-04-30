3ba3ad5616d032f651aba7221e7ac19a4027bb9e
DevCache1 <martynasbaranskas@gmail.com>
2024-04-30 11:36:07 +0300

    Initial Commit

---
+++ KaaS/KaaSApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaaSApp.swift
+//  KaaS
+//
+//  Created by Martynas Baranskas on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct KaaSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
